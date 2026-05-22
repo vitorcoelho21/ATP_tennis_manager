917d769d00a58aa5c7b830ee93da8b7e5c234f00
vitorcoelho21 <coelhocostavitor@gmail.com>
2026-05-22 18:30:53 -0300

    Menu torneio implementado

--- interfaces/menu_torneios.py
+++ interfaces/menu_torneios.py
@@ -1,3 +1,6 @@
+from models.Torneio import Torneio
+
+
 class MenuTorneios:
     def __init__(self, sistemaATP):
         self.sistema_atp = sistemaATP
@@ -23,3 +26,23 @@ class MenuTorneios:
                 break
             else:
                 print("Opção inválida. Tente novamente.")
+
+    def criar_torneio(self):
+        nome = input("Digite o nome do torneio: ")
+        superficie = input("Digite a superfície do torneio (saibro, grama, hard): ")
+        categoria = input("Digite a categoria do torneio: ")
+        if superficie == "saibro":
+            torneio = Torneio(nome, superficie, categoria)
+            self.sistema_atp.torneios.append(torneio)
+            print(f"Torneio {nome} criado com sucesso!")
+        if superficie == "grama":
+            torneio = Torneio(nome, superficie, categoria)
+            self.sistema_atp.torneios.append(torneio)
+            print(f"Torneio {nome} criado com sucesso!")
+        if superficie == "hard":
+            torneio = Torneio(nome, superficie, categoria)
+            self.sistema_atp.torneios.append(torneio)
+            print(f"Torneio {nome} criado com sucesso!")
+        else:
+            print("Superfície inválida. Tente novamente.")
+        
